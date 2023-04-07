491170d61a9abc426b758306fe98487edd3cb238
Marcos Valeriano <marcosfov@outlook.com>
2023-04-07 20:41:46 -0300

    Add styled-components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // problema do babel com SWC
+  experimental: {
+    forceSwcTransforms: true
+  }
 }
 
 module.exports = nextConfig
