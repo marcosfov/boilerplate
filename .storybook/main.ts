a5360882549bfe7047f6eaae5488753263e92a01
Marcos Valeriano <marcosfov@outlook.com>
2023-04-11 10:31:59 -0300

    Adicionado autodocs do storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs'
 const config: StorybookConfig = {
-  stories: ['../src/components/**/stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-knobs'
-  ],
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/nextjs',
     options: {}
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,4 +1,3 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import type { Meta, StoryObj } from '@storybook/react'
 
 import Main from '.'
@@ -6,32 +5,24 @@ import Main from '.'
 const meta = {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  tags: ['autodocs']
 } as Meta<typeof Main>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Basic: Story = {
-  render: () => (
-    <Main
-      title={text('Title', 'React Avançado')}
-      description={text(
-        'Description',
-        'TypeScript, ReactJS, NextJS e Styled Components'
-      )}
-    />
-  )
+  render: (args) => <Main {...args} />
+}
+Basic.args = {
+  title: 'asijdfioajsdifj',
+  description: 'asdhfoiasho'
 }
 
-export const Secondary: Story = {
-  render: () => (
-    <Main
-      title={text('Title', 'React Avançado123')}
-      description={text(
-        'Description',
-        'TypeScript, ReactJS, NextJS e Styled Components'
-      )}
-    />
-  )
+export const Default: Story = {
+  render: (args) => <Main {...args} />
+}
+Default.args = {
+  title: '123',
+  description: '456321'
 }
